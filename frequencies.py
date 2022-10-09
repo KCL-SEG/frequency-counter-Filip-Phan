9b9a140b01e095e19b3b17999f7e2ef81f804779
Huy Phan <duc.phan@kcl.ac.uk>
2022-10-09 18:25:47 +0100

    Do exercise

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,9 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        str_item = str(item)
+        if (str_item not in frequencies):
+            frequencies[str_item] = 0
+        frequencies[str_item] += 1
     return frequencies
